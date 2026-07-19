35c53b95c920680d6b4e7bb688fcbe6ce521722d
Salim Shrestha <113753193+salimshre@users.noreply.github.com>
2026-07-19 08:38:43 +0545

    Update start-challenges-app.bat

--- start-challenges-app.bat
+++ start-challenges-app.bat
@@ -1,6 +1,20 @@
 @echo off
+title PHP Local Server
+
 cd /d "C:\Users\StudyAcer\Documents\GitHub\21st"
+
 echo Starting PHP server at http://localhost:8000
-echo Press Ctrl+C to stop.
-C:\xampp\php\php.exe -S localhost:8000
+
+:: Start PHP server in a new window
+start "PHP Server" C:\xampp\php\php.exe -S localhost:8000
+
+:: Wait 2 seconds for the server to start
+timeout /t 2 /nobreak >nul
+
+:: Open the default browser
+start http://localhost:8000
+
+echo.
+echo Server is running.
+echo Close the "PHP Server" window to stop the server.
 pause
